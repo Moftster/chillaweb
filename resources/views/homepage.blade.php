b676e7f14a4ab01642ba0daa26f78822ad28cd07
David Moftakhar <david@sellmysnaps.com>
2020-02-19 12:09:21 +0000

    Image included and re-sized.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,7 +8,22 @@
 
 @section('content')
 
-<h1>This is the home page</h1>
+<div class="jumbotron">
+    <div class="row">
+        <div class="col-md-6">
+            <p class="lead">UK Website Design and Development</p>
+            <h4 class="display-4">Custom websites from £999</h4>
+            <hr class="my-4">
+            <p class="lead">Quality design   |   UK-based studio</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Pricing</a>
+            <a class="btn btn-outline-primary btn-lg" href="#" role="button">Portfolio</a>
+            </p>
+        </div>
+        <div class="col-md-6">
+            <img src="{{ asset('img/server-woman.png') }}" class="img-responsive fit-image" alt="Woman coding">
+        </div>
+    </div>
+</div>
 
 @endsection
 
