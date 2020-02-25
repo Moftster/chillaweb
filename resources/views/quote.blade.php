aa00d07c64822a21ea84f66109cbcdaa21a6ea5d
David Moftakhar <david@sellmysnaps.com>
2020-02-25 12:11:27 +0000

    Purpose section in quotes updated.

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -16,37 +16,72 @@
 </div>
 
 <br>
-
-<div></div>
-
-
+<br>
+<br>
 
 <form action="">
-    <h5 class="text-center">What do you need your webiste to do?</h5>
+    <h5 class="text-center"><strong>What is the purpose of your site?</strong></h5>
     <p class="text-center">(Select all that apply)</p>
 <div class="row text-center icons-center">
     <ul>
-        <li><input type="checkbox" id="cb-sales-marketing" />
+        <li><input type="checkbox" name="site-purpose" value="sales-marketing" id="cb-sales-marketing" />
           <label for="cb-sales-marketing"><img src="{{ asset('icons/showcase2.png') }}" /></label>
           <div class="label-text text-center">Sales & Marketing </div>
         </li>
-        <li><input type="checkbox" id="cb-information-source" />
+        <li><input type="checkbox" name="site-purpose" value="information-source" id="cb-information-source" />
           <label for="cb-information-source"><img src="{{ asset('icons/showcase2.png') }}" /></label>
           <div class="label-text text-center">Information source</div>
         </li>
-        <li><input type="checkbox" id="cb-lead-generation" />
+        <li><input type="checkbox" name="site-purpose" value="lead-generation" id="cb-lead-generation" />
           <label for="cb-lead-generation"><img src="{{ asset('icons/showcase2.png') }}" /></label>
           <div class="label-text text-center">Lead generation</div>
         </li>
-        <li><input type="checkbox" id="cb-e-commerce" />
+        <li><input type="checkbox" name="site-purpose" value="e-commerce" id="cb-e-commerce" />
           <label for="cb-e-commerce"><img src="{{ asset('icons/showcase2.png') }}" /></label>
           <div class="label-text text-center">E-commerce</div>
         </li>
-        <li><input type="checkbox" id="cb-bookings-reservations" />
+        <li><input type="checkbox" name="site-purpose" value="bookings-reservations" id="cb-bookings-reservations" />
           <label for="cb-bookings-reservations"><img src="{{ asset('icons/showcase2.png') }}" /></label>
           <div class="label-text text-center">Bookings & Reservations</div>
         </li>
     </ul>
+</div>
+
+<br><br><br>
+
+<h5 class="text-center"><strong>What functionality to you require?</strong></h5>
+<p class="text-center">(Select all that apply)</p>
+<div class="row text-center icons-center">
+<ul>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-login-registration" />
+      <label for="cb-login-registration"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">User login & registration</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-shopping-cart" />
+      <label for="cb-shopping-cart"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Shopping cart</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-live-chat" />
+      <label for="cb-live-chat"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Live chat</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-blog" />
+      <label for="cb-blog"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Blog</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-forum" />
+      <label for="cb-forum"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Forum</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-online-bookings" />
+      <label for="cb-online-bookings"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Online bookings</div>
+    </li>
+    <li><input type="checkbox" name="site-functionality" value="sales-marketing" id="cb-calendar" />
+      <label for="cb-calendar"><img src="{{ asset('icons/showcase2.png') }}" /></label>
+      <div class="label-text text-center">Calendar</div>
+    </li>
+</ul>
 </div>
 
 
