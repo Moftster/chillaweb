a52fa418446ce4e69de32bf87ae8e448ee98445a
David Moftakhar <david@sellmysnaps.com>
2020-03-02 08:31:15 +0000

    Quote migration created.

---
+++ database/migrations/2020_03_02_082335_create_quotes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('purpose');
+            $table->string('functionality');
+            $table->string('brand');
+            $table->string('timescale');
+            $table->integer('cost');
+            $table->string('name');
+            $table->string('email');
+            $table->string('telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
