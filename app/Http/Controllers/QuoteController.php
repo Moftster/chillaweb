17df4ab4f7c4128d297f3a618cb0c1cc51e0256f
David Moftakhar <david@sellmysnaps.com>
2020-03-04 09:00:20 +0000

    Quote form sending details via email.

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 // Import Quote model
 Use App\Quote;
 
+// Use Mail
+use Mail;
+
 class QuoteController extends Controller
 {
     public function index()
@@ -52,6 +55,31 @@ class QuoteController extends Controller
         $quote->telephone = $req->contactPhone;
 
         $quote->save();
+
+        // Send email to admin once quote form submitted
+        $emailTo = "David Moftakhar";
+        $emailAddress = "david@chillaweb.com";
+        $contactName = $req->contactName;
+        $contactEmail = $req->contactEmail;
+        $contactPhone = $req->contactPhone;
+        $siteBrand = $req->site_style;
+        $proposedTimescale = $req->site_timescale;
+        $data = array(  'name'=> $contactName, 
+                        'email'=> $contactEmail, 
+                        'phone' => $contactPhone,
+                        'purpose' => $purpose,
+                        'functionality' => $functionality,
+                        'quote' => $estimatedPrice,
+                        'brand' => $siteBrand,
+                        'timescale' => $proposedTimescale
+                    );
+        
+        // Mail to ChillaWeb
+        Mail::send('email/quotesubmission', $data, function($message) use ($emailTo, $emailAddress){
+            $message->to($emailAddress)
+            ->subject('ChillaWeb quote submission');
+        // echo "Email sent";
+        });
 
         $req->session()->flash('success', 'The details of your quote have been sent and you will receive a response shortly.');
 
---
+++ resources/views/email/quotesubmission.blade.php
@@ -0,0 +1,22 @@
+<strong>Name:</strong> {{$name}} 
+<br>
+<br>
+<strong>Email:</strong> {{$email}} 
+<br>
+<br>
+<strong>Phone:</strong> {{$phone}} 
+<br>
+<br>
+<strong>Purpose:</strong> {{$purpose}} 
+<br>
+<br>
+<strong>Functionality:</strong> {{$functionality}} 
+<br>
+<br>
+<strong>Brand:</strong> {{$brand}} 
+<br>
+<br>
+<strong>Timescale:</strong> {{$timescale}} 
+<br>
+<br>
+<strong>Quote:</strong> £{{$quote}} 
