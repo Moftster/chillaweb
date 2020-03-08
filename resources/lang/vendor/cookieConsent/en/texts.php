e1cd51faf20376ef7147ac175ae5cd86f809f6f3
David Moftakhar <david@sellmysnaps.com>
2020-03-08 17:06:53 +0000

    Cookies banner included.

---
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'This website uses cookies to ensure you get the best possible experience. <a href="https://www.cookiesandyou.com/">Learn more</a>.',
+    'agree' => 'Got it!',
+];
